edeacf92b1f0ab34b775592060bbe2343cbc708e
Wai Min Hein <wai06276@gmail.com>
2024-01-31 21:53:18 +0630

    home sketch design

---
+++ src/components/Download.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import downloadPNg from '../../public/images/download.png'
+import google from '../../public/images/googleDl.png'
+import appStore from '../../public/images/appStoreDl.png'
+import Image from 'next/image'
+
+const Download = () => {
+  return (
+    <section className='container mx-auto h-screen flex items-center justify-start gap-6'>
+
+        <div className="relative h-[90vh] basis-1/2">
+            <Image alt='image' src={downloadPNg} objectFit='contain' layout='fill' className='relative block'/>
+        </div>
+
+        <div className="basis-2/5">
+            <p>Download app</p>
+            <h1 className='text-4xl'>Download app for your smart phone now</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam nam, dicta deserunt, illo corrupti officia molestias possimus tempora laborum distinctio.
+            </p>
+
+            <div className="flex items-center justify-between w-2/3 mt-8">
+                <div className="basis-1/2 relative h-12">
+                    <Image alt='Image' src={google} layout='fill' objectFit='contain' className='relative block'/>
+                </div>
+
+                <div className="basis-1/2 relative h-12">
+                    <Image alt='Image' src={appStore} layout='fill' objectFit='contain' className='relative block'/>
+                </div>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Download
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='container mx-auto'>
+        <h1>Footer</h1>
+    </section>
+  )
+}
+
+export default Footer
