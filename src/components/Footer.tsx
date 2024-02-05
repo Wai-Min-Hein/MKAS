d019ecc4dfb3537b9c9436e94eac93ba7e8e4d79
Wai Min Hein <wai06276@gmail.com>
2024-02-05 16:41:04 +0630

    responsive 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,34 +4,33 @@ import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <section className="  py-8">
-        <div className="container mx-auto flex items-start justify-between border-b-[.5px] border-b-gray-700 pb-4">
-          <div className="basis-1/2">
-            <div className="">
-              <h1>Myanmar KAS Technology</h1>
-            </div>
-            <p className="mt-4">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Aspernatu incidunt
-              itaque vitae corporis fugiat est quae dolore eum, lib.
-            </p>
-
-            <div className="flex items-start justify-start gap-4 mt-4">
-              <FaFacebook className="text-2xl" />
-              <FaYoutube className="text-2xl" />
-              <MdEmail className="text-2xl" />
-            </div>
+    <section className="py-8">
+      <div className="container mx-auto flex items-start justify-between border-b-[.5px] border-b-gray-700 pb-4">
+        <div className="basis-1/2">
+          <div className="">
+            <h1>Myanmar KAS Technology</h1>
           </div>
-          <div className="basis-1/2  container mx-auto">
-            <h1 className="text-center">More Details</h1>
+          <p className="mt-4">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatu
+            incidunt itaque vitae corporis fugiat est quae dolore eum, lib.
+          </p>
 
-            <div className="flex items-center justify-center gap-16 mt-4">
-              <p>About us</p>
-              <p>Services</p>
-              <p>Contact us</p>
-            </div>
+          <div className="flex items-start justify-start gap-4 mt-4">
+            <FaFacebook className="text-2xl" />
+            <FaYoutube className="text-2xl" />
+            <MdEmail className="text-2xl" />
           </div>
         </div>
+        <div className="basis-1/2  container mx-auto">
+          <h1 className="text-center">More Details</h1>
+
+          <div className="flex flex-wrap items-center justify-center gap-16 mt-4">
+            <p>About us</p>
+            <p>Services</p>
+            <p>Contact us</p>
+          </div>
+        </div>
+      </div>
 
       <div className="container mx-auto mt-2">
         <h6>Develop by Myanmar KAS Technology</h6>
